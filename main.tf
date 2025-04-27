09001818c110ff04a61c61dc854940639b738362
KruPeter <krumerpeter@gmail.com>
2025-04-27 15:05:23 +0300

    cleaned sg

--- main.tf
+++ main.tf
@@ -32,7 +32,8 @@ resource "aws_instance" "web" {
 
 module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "5.3.0"
+  name    = "web_sg"
 
   vpc_id              = data.aws_vpc.default.id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
@@ -40,37 +41,4 @@ module "web_sg" {
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-
-  tags = {
-    name = "web_sg"
-  }
 }
-
-resource "aws_security_group" "web_sg" {
-  name        = "web_sg"
-  description = "Allow HTTP and https traffic"
-
-  vpc_id = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
