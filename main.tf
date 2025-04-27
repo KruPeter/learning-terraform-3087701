b92515439f5e88643d29efc202c6476d7563ed50
KruPeter <krumerpeter@gmail.com>
2025-04-27 14:44:26 +0300

    module added

--- main.tf
+++ main.tf
@@ -23,10 +23,26 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  vpc_security_group_ids = [module.web_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+module "web_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id              = data.aws_vpc.default.id
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  tags = {
+    name = "web_sg"
   }
 }
 
